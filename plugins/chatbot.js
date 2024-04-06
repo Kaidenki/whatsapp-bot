6b4abc2164ea7565cb719f3706822300bcf89c4b
Obasuyi Promise <156423967+C-iph3r@users.noreply.github.com>
2024-04-06 20:08:51 +0100

    Update chatbot.js

--- plugins/chatbot.js
+++ plugins/chatbot.js
@@ -1,44 +1,30 @@
-/*const { Alpha, getJson, config, mode } = require('../lib');
-let prefix = config.CHATPX !== null ? config.CHATPX : (config.PREFIX !== undefined ? config.PREFIX : null);
+const {
+        Alpha,
+        getJson,
+        config,
+        mode
+} = require('../lib');
 
-    Alpha({
+
+Alpha({
         on: 'text',
         fromMe: mode
-    }, async (m, match) => {
-        if (!config.CHATBOT) {
-    return;
-} 
-        if (!prefix && (config.CHATBOT === 'pm2' || config.CHATBOT === 'gc2')) {
-                await m.reply("The chatbot is not properly configured to require a prefix. Please set  prefix.");
-                return;
-            }    
-        if (config.CHATBOT === 'pm2' && m.isCreator) {
-            if (!m.body.startsWith(prefix)) return;
+}, async (m, match) => {
+        if(m.isCreator && config.CHATBOT == 'pm2' ) return;
+        if(config.CHATBOT == 'true') {
+                let data = await getJson(
+                        `http://api.brainshop.ai/get?bid=${config.BRAINSHOP.split(/[,;|]/)[0]}&key=${config.BRAINSHOP.split(/[,;|]/)[1]}&uid=[${m.sender.split('@')[0]}]&msg=[${m.body}]`
+                )
+                return await m.reply(data.cnt)
+        } else if(config.CHATBOT == 'group' && m.isGroup) {
+                let data = await getJson(
+                        `http://api.brainshop.ai/get?bid=${config.BRAINSHOP.split(/[,;|]/)[0]}&key=${config.BRAINSHOP.split(/[,;|]/)[1]}&uid=[${m.sender.split('@')[0]}]&msg=[${m.body}]`
+                )
+                return await m.reply(data.cnt)
+        } else if(config.CHATBOT == 'pm' && !m.isGroup) {
+                let data = await getJson(
+                        `http://api.brainshop.ai/get?bid=${config.BRAINSHOP.split(/[,;|]/)[0]}&key=${config.BRAINSHOP.split(/[,;|]/)[1]}&uid=[${m.sender.split('@')[0]}]&msg=[${m.body}]`
+                )
+                return await m.reply(data.cnt)
         }
-        if (config.CHATBOT === 'pm3' && m.isCreator && !m.isGroup) return;
-        if (config.CHATBOT === 'gc2' && m.isCreator && m.isGroup) {
-            if (!m.body.startsWith(prefix)) return;
-        }
-        if (config.CHATBOT === 'gc3' && m.isCreator && m.isGroup) return;
-        if (config.CHATBOT === 'gc' && m.isGroup) {
-            let msg = m.body.startsWith(prefix) ? m.body.slice(prefix.length) : m.body;
-            console.log(msg)
-            let data = await getJson(
-                `http://api.brainshop.ai/get?bid=${config.BRAINSHOP.split(/[,;|]/)[0]}&key=${config.BRAINSHOP.split(/[,;|]/)[1]}&uid=[${m.sender.split('@')[0]}]&msg=[${msg}]`
-            );
-            return await m.reply(data.cnt);
-        }
-        if ((config.CHATBOT === 'pm' || !m.isGroup) && m.isCreator) {
-            let msg = m.body.startsWith(prefix) ? m.body.slice(prefix.length) : m.body;
-            let data = await getJson(
-                `http://api.brainshop.ai/get?bid=${config.BRAINSHOP.split(/[,;|]/)[0]}&key=${config.BRAINSHOP.split(/[,;|]/)[1]}&uid=[${m.sender.split('@')[0]}]&msg=[${msg}]`
-            );
-            return await m.reply(data.cnt);
-        }
-        let msg = m.body.startsWith(prefix) ? m.body.slice(prefix.length) : m.body;
-        let data = await getJson(
-            `http://api.brainshop.ai/get?bid=${config.BRAINSHOP.split(/[,;|]/)[0]}&key=${config.BRAINSHOP.split(/[,;|]/)[1]}&uid=[${m.sender.split('@')[0]}]&msg=[${msg}]`
-        );
-        return await m.reply(data.cnt);
-    });*/
-    
+});
