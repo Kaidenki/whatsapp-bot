317f746182e71f959088d377dbcb5f69b108ceaf
Obasuyi Promise <156423967+C-iph3r@users.noreply.github.com>
2024-04-10 13:48:47 +0100

    Update apk.js

--- plugins/apk.js
+++ plugins/apk.js
@@ -7,15 +7,15 @@ Alpha({
 	fromMe: mode
 }, async (message, match) => {
 	match = match || message.reply_message.text;
-	if (!match) return await message.send("*please give me an application name*");
+	if (!match) return await message.reply("*please give me an application name*");
 	if (match) {
 		match = match
 		const res = await getJson(`${config.BASE_URL}api/dowloader/apk?text=${match}&apikey=${config.ALPHA_KEY}`);
-		if (!res.status) return await message.send(`file not found or  apikey limit has been exceeded. Visit ${config.BASE_URL}signup for gettig a new apikey. setvar alpha_key: your apikey`);
+		if (!res.status) return await message.reply(`file not found or  apikey limit has been exceeded. Visit ${config.BASE_URL}signup for gettig a new apikey. setvar alpha_key: your apikey`);
 		await new Promise(resolve => setTimeout(resolve, 1000));
 		await message.sendReply(res.result.icon, { caption: `*Name*: \`${res.result.name}\`\n*Updated*: ${res.result.lastup}\n*Package*: ${res.result.package}\n*Size*: ${res.result.size}` }, 'image');
 		await new Promise(resolve => setTimeout(resolve, 1000));
-		return await message.send({
+		return await message.reply({
 			url: res.result.dllink
 		}, {
 			mimetype: `application/vnd.android.package-archive`,
@@ -35,17 +35,17 @@ Alpha({
     fromMe: mode
 }, async (message, match) => {
     match = match || message.reply_message.text;
-    if (!match) return await message.send('*please provide a search query*');
+    if (!match) return await message.reply('*please provide a search query*');
     const res = await getJson(`${config.BASE_URL}api/search/apk?text=${match}&apikey=${config.ALPHA_KEY}`);
-    if (!res.status) return await message.send('No results found.');
+    if (!res.status) return await message.reply('No results found.');
     const results = res.result;
-    if (results.length === 0) return await message.send('No results found.');
+    if (results.length === 0) return await message.reply('No results found.');
     let replyMsg = '*Search Results:*\n';
     results.forEach((app, index) => {
         replyMsg += `${index + 1}. ${app.name}\n`;
     });
     replyMsg += '*Reply with the number to download the corresponding APK file.*\n*or use 0 to cancel download*';
-    await message.send(replyMsg);
+    await message.reply(replyMsg);
     listeningForSAPK = true;
     selectedAppInfo = results;
 });
@@ -59,13 +59,13 @@ Alpha({
     if (selectedAppInfo && selectedAppInfo.length > 0 && !isNaN(parseInt(message.body))) {
         const selection = parseInt(message.body)
         if (selection === 0) { 
-            await message.send("*Download canceled.*");
+            await message.reply("*Download canceled.*");
             listeningForSAPK = false;
         } else 	if (selection >= 1 && selection <= selectedAppInfo.length) {
             const selectedApp = selectedAppInfo[selection - 1];
             const downloadRes = await getJson(`${config.BASE_URL}api/dowloader/apk?text=${selectedApp.name}&apikey=${config.ALPHA_KEY}`);
             if (!downloadRes.status) {
-                await message.send(`*Error downloading ${selectedApp.name}: API key limit exceeded.*`);
+                await message.reply(`*Error downloading ${selectedApp.name}: API key limit exceeded.*`);
                 return;
             }
             await new Promise(resolve => setTimeout(resolve, 1000));
@@ -73,7 +73,7 @@ Alpha({
                 caption: `*Name*: \`${selectedApp.name}\`\n*Package*: \`${selectedApp.id}\``
             }, 'image');
             await new Promise(resolve => setTimeout(resolve, 1000));
-            await message.send({
+            await message.reply({
                 url: downloadRes.result.dllink
             }, {
                 mimetype: 'application/vnd.android.package-archive',
@@ -82,7 +82,7 @@ Alpha({
             listeningForSAPK = false;
             selectedAppInfo = null;
         } else {
-            await message.send("*Invalid selection. Please enter a valid number. Please enter a number from 1 to ${selectedAppInfo.length}.*");
+            await message.reply("*Invalid selection. Please enter a valid number. Please enter a number from 1 to ${selectedAppInfo.length}.*");
         }
     }
 });
