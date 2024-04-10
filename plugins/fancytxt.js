435a11cb01bedabaf954d788b3538f3d1e816618
Obasuyi Promise <156423967+C-iph3r@users.noreply.github.com>
2024-04-11 00:46:20 +0100

    fied a minor issue

--- plugins/fancytxt.js
+++ plugins/fancytxt.js
@@ -2,6 +2,8 @@ const { Alpha, mode, getJson, config } = require('../lib');
 
 let fancycheck = false;
 let fancytimeout;
+let options;
+const regex = /^[a-zA-Z0-9 ]+$/;
 
 Alpha({
     pattern: 'fancy ?(.*)',
@@ -9,14 +11,17 @@ Alpha({
     desc: 'Style your text creatively.',
     fromMe: mode
 }, async (message, match) => {
-    if (!match) return await message.reply('_please can you provide me a text_');
+    if (!match) return await message.reply('_please provide a text_');
+    if (!regex.test(match)) {
+        return await message.reply('_please provide a valid text_');
+    }
     const res = await getJson(`${config.BASE_URL}api/tools/styletext?text=${match}&apikey=${config.ALPHA_KEY}`);
-    if (!res.status) return await message.reply(`*Failed to fetch fancy text options.*\n*please make sure i'ts plain text*`);
+    if (!res.status) return await message.reply(`*API key limit exceeded. Get a new API key at ${config.BASE_URL}signup. Set var alpha_key: your_api_key*`);
     
-    const options = res.result;
-    if (options.length === 0) return await message.reply('*No fancy text options available.*');
+    options = res.result;
+    if (options.length === 0) return await message.reply('*Sorry, fancy text is temporarily unavailable.*');
 
-    let replyMsg = '*fancy Text Options:*\n';
+    let replyMsg = '*Fancy Text Options:*\n';
     options.forEach((option, index) => {
         replyMsg += `${index + 1}. ${option.result}\n`;
     });
@@ -27,28 +32,31 @@ Alpha({
     fancytimeout = setTimeout(() => {
         fancycheck = false;
     }, 60000); // 1 minute timeout
+});
 
-    Alpha({
-        on: 'text',
-        fromMe: mode
-    }, async (message, match) => {
-        if (!fancycheck) return;
-        if (!message.reply_message?.fromMe || !message.reply_message?.text) return;
-        if (!message.reply_message.text.includes('*Reply with the number of your desired fancy text.*\n*or 0 to cancel.*')) return;
-        const selection = parseInt(message.body);
-        if (isNaN(selection) || selection < 1 || selection > options.length) {
-            await message.reply('Invalid selection. Please reply with a valid number.');
-            return;
-        }
-        if (selection === 0) {
-            await message.reply('Operation cancelled.');
-            clearTimeout(stylizeTimeout);
-            fancytimeout = false;
-            return;
-        }
+Alpha({
+    on: 'text',
+    fromMe: mode
+}, async (message, match) => {
+    if (!fancycheck) return;
+    if (!message.reply_message?.fromMe || !message.reply_message?.text) return;
+    if (!message.reply_message.text.includes('*Reply with the number of your desired fancy text.*\n*or 0 to cancel.*')) return;
+    const selection = parseInt(message.body);
+    if (fancycheck && selection === 0) {
+        await message.reply('*Operation cancelled.*');
+        clearTimeout(fancytimeout);
+        fancytimeout = false;
+        fancycheck = false; 
+        return;
+    }
+    if (fancycheck && (isNaN(selection) || selection < 1 || selection > options.length)) {
+        await message.reply('*Invalid selection. Please reply with a valid number.*');
+        return;
+    }
+    if (fancycheck) {
         const selectedOption = options[selection - 1].result;
         await message.reply(`${selectedOption}`);
         clearTimeout(fancytimeout);
-        fancycheck = false;
-    });
+        fancycheck = false; 
+    }
 });
