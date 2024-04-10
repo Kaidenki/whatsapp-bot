74a04f551021ad3f6ac4e3bd9e522d9116f8800e
Obasuyi Promise <156423967+C-iph3r@users.noreply.github.com>
2024-04-10 13:47:59 +0100

    Update fancytxt.js

--- plugins/fancytxt.js
+++ plugins/fancytxt.js
@@ -9,18 +9,19 @@ Alpha({
     desc: 'Style your text creatively.',
     fromMe: mode
 }, async (message, match) => {
+    if (!match) return await message.reply('_please can you provide me a text_');
     const res = await getJson(`${config.BASE_URL}api/tools/styletext?text=${match}&apikey=${config.ALPHA_KEY}`);
-    if (!res.status) return await message.send(`*Failed to fetch fancy text options.*\n*please make sure i'ts plain text*`);
+    if (!res.status) return await message.reply(`*Failed to fetch fancy text options.*\n*please make sure i'ts plain text*`);
     
     const options = res.result;
-    if (options.length === 0) return await message.send('*No fancy text options available.*');
+    if (options.length === 0) return await message.reply('*No fancy text options available.*');
 
     let replyMsg = '*fancy Text Options:*\n';
     options.forEach((option, index) => {
         replyMsg += `${index + 1}. ${option.result}\n`;
     });
     replyMsg += '*Reply with the number of your desired fancy text.*\n*or 0 to cancel.*';
-    await message.send(replyMsg);
+    await message.reply(replyMsg);
 
     fancycheck = true;
     fancytimeout = setTimeout(() => {
@@ -32,20 +33,20 @@ Alpha({
         fromMe: mode
     }, async (message, match) => {
         if (!fancycheck) return;
-     //   if (!message.sender) return;
+      //  if (!message.sender) return;
         const selection = parseInt(message.body);
         if (isNaN(selection) || selection < 1 || selection > options.length) {
-            await message.send('Invalid selection. Please reply with a valid number.');
+            await message.reply('Invalid selection. Please reply with a valid number.');
             return;
         }
         if (selection === 0) {
-            await message.send('Operation cancelled.');
+            await message.reply('Operation cancelled.');
             clearTimeout(stylizeTimeout);
             fancytimeout = false;
             return;
         }
         const selectedOption = options[selection - 1].result;
-        await message.send(`${selectedOption}`);
+        await message.reply(`${selectedOption}`);
         clearTimeout(fancytimeout);
         fancycheck = false;
     });
