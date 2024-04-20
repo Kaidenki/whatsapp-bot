fa98c0625dc66d70de9305ad225aafac5499059e
Obasuyi Promise <156423967+C-iph3r@users.noreply.github.com>
2024-04-20 16:33:41 +0100

    FEAT: Updated gemini cmd

--- plugins/gpt.js
+++ plugins/gpt.js
@@ -15,7 +15,7 @@ Alpha({
         if(!config.OPEN_AI) {
             const res = await getJson(`${config.BASE_URL}api/ai/gpt1?prompt=${encodeURIComponent(match)}&apikey=${config.ALPHA_KEY}`);
            if (res.error) return await message.send(`Please enter a new apikey, as the given apikey limit has been exceeded. Visit ${config.BASE_URL}signup for gettig a new apikey. setvar alpha_key: your apikey`);
-            return await message.send(res.result.reply);
+            return await message.reply(res.result.reply);
         } 
         return await message.send(await GPT.prompt(match));
 });
@@ -29,11 +29,10 @@ Alpha({
     match = match || message.reply_message.text;
     if (!match) return await message.reply('_please can you provide me a task_');
     const geminiUrl = `${config.API_URL}api/gemini?prompt=${encodeURIComponent(match)}&api_key=${config.GEMINI_KEY}`;
-    
     const res = await getJson(geminiUrl);
     if (res.error) {
         return await message.send(`*There was an issue processing your request. Please try again later or provide a different prompt.*`);
     } 
     const text = res.candidates[0].content.parts[0].text;
-    return await message.send(text);
+    return await message.reply(text);
 });
