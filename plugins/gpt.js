e6ea25e223c01727c4b63d36cb9191adfd5849bf
Obasuyi Promise <156423967+C-iph3r@users.noreply.github.com>
2024-04-20 16:23:00 +0100

    FEAT: Added gemini ai cmd

--- plugins/gpt.js
+++ plugins/gpt.js
@@ -1,4 +1,5 @@
 const { Alpha, GPT, mode, config,getJson} = require('../lib/');
+
 Alpha({
     pattern: "gpt",
     desc: 'get open ai chatgpt response',
@@ -17,4 +18,22 @@ Alpha({
             return await message.send(res.result.reply);
         } 
         return await message.send(await GPT.prompt(match));
+});
+
+Alpha({
+    pattern: "gemini",
+    desc: 'get gemini ai response',
+    type: "Ai",
+    fromMe: mode
+}, async (message, match) => {
+    match = match || message.reply_message.text;
+    if (!match) return await message.reply('_please can you provide me a task_');
+    const geminiUrl = `${config.API_URL}api/gemini?prompt=${encodeURIComponent(match)}&api_key=${config.GEMINI_KEY}`;
+    
+    const res = await getJson(geminiUrl);
+    if (res.error) {
+        return await message.send(`*There was an issue processing your request. Please try again later or provide a different prompt.*`);
+    } 
+    const text = res.result.candidates[0].content.parts[0].text;
+    return await message.send(text);
 });
